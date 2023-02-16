bc17b6e01389e79ead05eee87467be930d31b3a9
Xevion <ryanchwalters@gmail.com>
2023-02-16 14:53:54 -0600

    Use @ts-ignore by line

--- src/utils/iconList.ts
+++ src/utils/iconList.ts
@@ -1,10 +1,10 @@
 // @codegen
-// @ts-nocheck
-/* eslint-disable */
 
+/* eslint-disable */
 // @ts-ignore
 const identifiers = require("react-icons/lib").IconsManifest.map(pack => pack.id);
 const icons = identifiers
+// @ts-ignore
     .map(setId => Object.keys(require(`react-icons/${setId}/index`)).map(id => (
         `{id: "${id}", setId: "${setId}"}`
     )))
