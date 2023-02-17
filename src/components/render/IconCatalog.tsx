0e1c7f4af50d97db46b84129c67725e9f9673771
Xevion <ryanchwalters@gmail.com>
2023-02-17 02:18:27 -0600

    Improve scaling on multiple viewports
    - fix improper variable being used
    - extract useMemo into variable

--- src/components/render/IconCatalog.tsx
+++ src/components/render/IconCatalog.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const IconCatalog: FunctionComponent<Props> = ({icons}) => {
     return <div
-        className="flex flex-wrap rounded gap-y-4 p-1.5 rounded overflow-x-clip mb-5">{icons.map(({id, setId}) => {
+        className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-8 lg:grid-cols-9 xl:grid-cols-11 rounded gap-4 p-1.5 rounded overflow-x-clip mb-5">{icons.map(({id, setId}) => {
         return <DisplayIconSingle key={id + setId} id={id} setId={setId}/>
     })}
     </div>
